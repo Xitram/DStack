96f6a540b3d3993c1ab813969eb8bf3530d390b0
Xitram <gilbertdavid32@yahoo.com>
2017-06-05 20:29:08 +0200

    Updated settings for static root

--- dpile/settings.py
+++ dpile/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/personal/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 CKEDITOR_UPLOAD_PATH = "media/"
