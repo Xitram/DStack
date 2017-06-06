45fc446aa4c7bcd4b5bb318c8d4dbe2d49f5bbd0
Xitram <gilbertdavid32@yahoo.com>
2017-06-06 17:02:17 +0200

    updated settings for editor

--- dpile/settings.py
+++ dpile/settings.py
@@ -116,6 +116,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Standard',
+    },
+}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
