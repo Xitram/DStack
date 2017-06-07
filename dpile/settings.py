16442a88c3106b76669efd271efd4c77c2909b40
Xitram <gilbertdavid32@yahoo.com>
2017-06-07 02:10:02 +0200

    yep..

--- dpile/settings.py
+++ dpile/settings.py
@@ -134,7 +134,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'
 CKEDITOR_UPLOAD_PATH = "media/"
 
-CKEDITOR_BASEPATH = '/static/personal/ckeditor/ckeditor'
+CKEDITOR_BASEPATH = '/static/personal/ckeditor/ckeditor/ckeditor.js'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
