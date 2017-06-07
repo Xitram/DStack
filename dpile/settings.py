a0e12f14ffc938e14ed40f827067b17e6ee3f200
Xitram <gilbertdavid32@yahoo.com>
2017-06-07 17:40:49 +0200

    pasted some stuff for S3

--- dpile/settings.py
+++ dpile/settings.py
@@ -148,7 +148,7 @@ REDACTOR_UPLOAD_HANDLER = 'redactor.handlers.SimpleUploader'
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_STORAGE_BUCKET_NAME = 'dstackbucket'
 AWS_QUERYSTRING_AUTH = False
 AWS_HEADERS = {
   'Cache-Control': 'max-age=86400',
