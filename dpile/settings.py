236bb5bf4d5da26127708fd5cb37f17d909a93d9
Xitram <gilbertdavid32@yahoo.com>
2017-06-07 17:31:17 +0200

    pasted some stuff for S3

--- dpile/settings.py
+++ dpile/settings.py
@@ -146,6 +146,14 @@ REDACTOR_UPLOAD = 'media/'
 REDACTOR_UPLOAD_HANDLER = 'redactor.handlers.SimpleUploader'
 
 
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_QUERYSTRING_AUTH = False
+AWS_HEADERS = {
+  'Cache-Control': 'max-age=86400',
+}
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 
 import dj_database_url
