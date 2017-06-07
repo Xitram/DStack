734bad16d9ecb074c17751c12fdddfc1410ff320
Xitram <gilbertdavid32@yahoo.com>
2017-06-07 13:09:33 +0200

    moved admin template folder

--- dpile/settings.py
+++ dpile/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'personal',
+    'ckeditor',
+    'ckeditor_uploader',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ckeditor',
-    'ckeditor_uploader',
 ]
 
 MIDDLEWARE = [
